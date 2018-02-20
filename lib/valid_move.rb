a7e81c8936ebefbd0fb7f775f93cafe848bd462a
Mars Santos <mars@meetup.com>
2018-02-20 02:14:33 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,3 +10,6 @@ def valid_move?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  board[index] != " "
+end
